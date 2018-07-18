dcf9e53bd644aab48fa3ceac3f58dfe38209719e
Christopher Arnold <chrisarnold2010@gmail.com>
2018-07-18 13:53:42 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person 
+  def name= (name)
+    @name = name
+  end
+  
+  def name
+    @name 
+  end
+  
+  def job= (job)
+    @breed = job
+  end
+  
+  def job
+    @job
+  end
+  
+end
